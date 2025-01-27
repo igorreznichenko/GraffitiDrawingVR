4b985ea164f4b6e83c3d54690bc3d5292566a192
Ihor Reznichenko <igor7210ak@gmail.com>
2025-01-27 09:21:20 +0200

    feat(Box): create the prefab, add the logic

---
+++ Assets/Scripts/Runtime/Debug/FPSCounter.cs
@@ -0,0 +1,40 @@
+using TMPro;
+using UnityEngine;
+
+namespace GraffitiDrawingVR.Runtime.Debug
+{
+	public class FPSCounter : MonoBehaviour
+	{
+		[SerializeField]
+		private TMP_Text _text;
+
+		[SerializeField]
+		private float _refreshTime;
+
+		private float _timer;
+
+		private const string FPS_OUTPUT_FORMAT = "{0} FPS";
+
+		public void Show()
+		{
+			gameObject.SetActive(true);
+		}
+
+		public void Hide()
+		{
+			gameObject.SetActive(false);
+		}
+
+		private void Update()
+		{
+			if (Time.unscaledTime > _timer)
+			{
+				int fps = (int)(1f / Time.unscaledDeltaTime);
+
+				_text.text = string.Format(FPS_OUTPUT_FORMAT, fps);
+
+				_timer = Time.unscaledTime + _refreshTime;
+			}
+		}
+	}
+}
